33045078828ba68e08fc3950fe45b73741c7da7e
karol204 <krogalski204@gmail.com>
2021-10-19 22:13:49 +0100

    routes and room creating

---
+++ music_room/frontend/src/components/HomePage.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Link, Switch, Route, Redirect } from "react-router-dom"
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render () {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/'><p>This is HomePage</p></Route>
+                    <Route path='/join' component={RoomJoinPage}><p>This is Join Page</p></Route>
+                    <Route path='/create' component={CreateRoomPage}><p>This is Join Page</p></Route>
+
+
+                </Switch>
+            </Router>
+        )
+    }
+}
