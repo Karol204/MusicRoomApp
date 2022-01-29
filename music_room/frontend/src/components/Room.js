fe494cb30c9b4e8b5318d4f47537094d2feaf69c
karol204 <krogalski204@gmail.com>
2022-01-29 14:16:51 +0000

    spotify cd

--- music_room/frontend/src/components/Room.js
+++ music_room/frontend/src/components/Room.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import CreateRoomPage from './CreateRoomPage'
+import MusicPlayer from "./MusicPlayer";
 
 
 export default class Room extends Component {
@@ -13,7 +14,8 @@ export default class Room extends Component {
             guestCanPause: false,
             isHost: false,
             showSettings: false,
-            spotifyAuthenticated: false
+            spotifyAuthenticated: false,
+            song: {}
         };
         this.roomCode = this.props.match.params.roomCode;
         this.leaveBtnPressed = this.leaveBtnPressed.bind(this)
@@ -22,8 +24,17 @@ export default class Room extends Component {
         this.renderSettings = this.renderSettings.bind(this)
         this.getRoomDetails = this.getRoomDetails.bind(this)
         this.authenticateSpotify = this.authenticateSpotify.bind(this)
+        this.getCurrentSong = this.getCurrentSong.bind(this)
         this.getRoomDetails();
     }
+
+      componentDidMount() {
+        this.interval = setInterval(this.getCurrentSong, 1000);
+      }
+
+      componentWillUnmount() {
+        clearInterval(this.interval);
+      }
 
     getRoomDetails() {
         fetch("/api/get-room" + "?code=" + this.roomCode)
@@ -47,11 +58,32 @@ export default class Room extends Component {
     )
     }
 
+    getCurrentSong() {
+      fetch("/spotify/current-song")
+        .then((response) => {
+          if (!response.ok) {
+            return {};
+          } else {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.setState({ song: data });
+          console.log(data);
+        });
+    }
+
+
+
     authenticateSpotify() {
-      fetch('/spotify/is-autheticated').then((response) => response.json()).then((data)=> {
+      fetch('/spotify/is-autheticated')
+      .then((response) => response.json())
+      .then((data)=> {
         this.setState({ spotifyAuthenticated: data.status })
         if (!data.status) {
-          fetch('/spotify/get-auth-url').then((response) => response.json()).then((data) => {
+          fetch('/spotify/get-auth-url')
+          .then((response) => response.json())
+          .then((data) => {
             window.location.replace(data.url)
           })
         }
@@ -111,42 +143,28 @@ export default class Room extends Component {
 
     render() {
         
-          if (this.state.showSettings){
-            return this.renderSettings();
-          }
-          return (
-          <Grid container spacing={1}>
-            <Grid item xs={12} align="center">
-              <Typography variant="h4" component="h4">
-                Code: {this.roomCode}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-              <Typography variant="h6" component="h6">
-                Votes: {this.state.votesToSkip}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-              <Typography variant="h6" component="h6">
-                Guest Can Pause: {this.state.guestCanPause.toString()}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-              <Typography variant="h6" component="h6">
-                Host: {this.state.isHost.toString()}
-              </Typography>
-            </Grid>
-            {this.state.isHost ? this.renderSettingsButton() : null}
-            <Grid item xs={12} align="center">
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={this.leaveBtnPressed}
-              >
-                Leave Room
-              </Button>
-            </Grid>
+      if (this.state.showSettings) {
+        return this.renderSettings();
+      }
+      return (
+        <Grid container spacing={1}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h4" component="h4">
+              Code: {this.roomCode}
+            </Typography>
           </Grid>
+          <MusicPlayer {...this.state.song} />
+          {this.state.isHost ? this.renderSettingsButton() : null}
+          <Grid item xs={12} align="center">
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={this.leaveButtonPressed}
+            >
+              Leave Room
+            </Button>
+          </Grid>
+        </Grid>
         );
       }
     }
--- music_room/spotify/util.py
+++ music_room/spotify/util.py
@@ -1,8 +1,9 @@
+from rest_framework import response
 from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
 from .credentials import CLIENT_ID, CLIENT_SECRET
-from requests import post
+from requests import post, put, get
 
 
 BASE_URL = "https://api.spotify.com/v1/me/"
@@ -11,7 +12,6 @@ BASE_URL = "https://api.spotify.com/v1/me/"
 def get_user_tokens(session_key):
     user_tokens = SpotifyToken.objects.filter(user=session_key)
     if user_tokens.exists():
-        print(user_tokens)
         return user_tokens[0]
     else:
         return None
@@ -37,7 +37,10 @@ def update_or_create_user_tokens(session_key, access_token, token_type, expires_
 
 
 def is_spotify_authenticated(session_key):
+
     tokens = get_user_tokens(session_key)
+    print(tokens)
+
     if tokens:
         expiry = tokens.expires_in
         if expiry <= timezone.now():
@@ -47,13 +50,14 @@ def is_spotify_authenticated(session_key):
 
 
 def refresh_spotify_token(session_key):
+
     refresh_token = get_user_tokens(session_key).refresh_token
 
-    response = post('http://accounts.spotify.com/api/token', data={
+    response = post('https://accounts.spotify.com/api/token', data={
         'grant_type': 'refresh_token',
         'refresh_token': refresh_token,
         'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
+        'client_secret': CLIENT_SECRET
     }).json()
 
     access_token = response.get('access_token')
@@ -62,4 +66,24 @@ def refresh_spotify_token(session_key):
     refresh_token = response.get('refresh_token')
 
     update_or_create_user_tokens(
-        session_key, access_token, token_type, expires_in)
+        session_key, access_token, token_type, expires_in, refresh_token)
+
+
+def execute_spotify_api_request(session_id, endpoint, post_=False, put_=False):
+    tokens = get_user_tokens(session_id)
+    print(tokens.access_token)
+
+    headers = {'Content-Type': 'application/json',
+               'Authorization': "Bearer " + tokens.access_token}
+    print(tokens.access_token)
+    if post_:
+        post(BASE_URL + endpoint, headers=headers)
+    if put_:
+        put(BASE_URL + endpoint, headers=headers)
+
+    response = get(BASE_URL + endpoint, {}, headers=headers)
+
+    try:
+        return response.json()
+    except:
+        return {"Error": "Issue with request"}
